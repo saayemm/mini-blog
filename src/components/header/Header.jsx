d28c1bbe53a8ecc70657549f74d4afe46f4b427f
Saayemm <sayemdcc@gmail.com>
2025-01-10 16:54:40 +0600

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,10 @@
+import profile from '../../assets/profile.png'
 
 export default function Header() {
   return (
-    <div>
+    <div className='flex justify-between items-center m-2 border-b-2 p-4 max-w-[1240]'>
         <h1 className="text-3xl font-bold">Mini Blog</h1>
+        <img src={profile} alt="" />
     </div>
   )
 }
